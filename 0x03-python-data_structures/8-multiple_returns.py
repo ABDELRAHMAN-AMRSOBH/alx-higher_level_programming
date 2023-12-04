3e43cf2b8bdc18125093fe3a399335e5863a4aa8
ABDELRAHMAN-AMRSOBH <Abdelrahmanamr.egypt@gmail.com>
2023-12-04 13:25:59 -0800

    msg

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        c = None
+    else:
+        c = sentence[0]
+    tup = (length, c)
+    return tup
