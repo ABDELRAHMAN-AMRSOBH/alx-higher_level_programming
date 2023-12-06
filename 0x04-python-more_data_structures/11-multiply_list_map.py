87981f335ff2296c2358ab288261ab8364db8477
ABDELRAHMAN-AMRSOBH <Abdelrahmanamr.egypt@gmail.com>
2023-12-06 13:42:47 -0800

    msg

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    new = list
-    for i in my_list:
-        new += i * number
-    return new
+def multiply_list_map(my_list=[], number=0):
+    return(list(map(lambda x: x * number, my_list)))
