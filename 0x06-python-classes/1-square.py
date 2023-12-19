79bb5f36520f32c2984559d0dad25a5f2f019ecd
ABDELRAHMAN-AMRSOBH <Abdelrahmanamr.egypt@gmail.com>
2023-12-19 12:04:10 -0800

    msg

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,15 @@
-ass Square:
+#!/usr/bin/python3
+"""Square module."""
+
+
+
+class Square:
     """Defines a square"""
+
     def __init__(self, size):
-        """Initialises the data"""
+        """Constructor.
+
+        Args:
+            size: length of a side of the square.
+        """
         self.__size = size
